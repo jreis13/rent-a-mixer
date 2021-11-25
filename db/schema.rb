7163b9c83ec47997a1d9d57ac0b328dcd719330f
viktoriia katyukova <vok1829@gmail.com>
2021-11-25 15:31:19 +0000

    map done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_24_103119) do
+ActiveRecord::Schema.define(version: 2021_11_25_113433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 2021_11_24_103119) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_services_on_user_id"
   end
 
