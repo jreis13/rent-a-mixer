ec5c94c8baf5882b909ea3288964533110156969
Joao Reis <joaofranciscoreis13@outlook.com>
2021-11-25 17:24:36 +0000

    fixing

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -33,7 +33,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/navigation-night-v1'
+      style: 'mapbox://styles/mapbox/streets-v11'
     });
     const markers = JSON.parse(mapElement.dataset.markers);
 
